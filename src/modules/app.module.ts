edcc7a196dca1a2806861de566f954be88bc0555
tayanaris <hialuron92@gmail.com>
2022-01-17 21:50:14 +0300

    postgresql config fix

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 @Module({
   imports: [
 	TypeOrmModule.forRoot({
 		type: 'postgres',
 		host: 'localhost',
 		port: 5432,
-		username: 'test',
+		username: 'postgres',
 		password: 'test',
 		database: 'exchange_market',
 		entities: [],
 		synchronize: true,
 	}),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
-export class AppModule {}
+
+export class AppModule {
+	constructor (private connection: Connection) {}
+}
